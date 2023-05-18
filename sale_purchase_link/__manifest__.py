4a85f6dbb8d4289bd9b465fffba5253319a14ab7
jbasiero <jbasiero@gmail.com>
2023-05-18 17:27:45 +0200

    sale_purchase_link

---
+++ sale_purchase_link/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale & Purchase link",
+    'summary': """This module add new buttons to show the sale in the purchase and vice versa""",
+    'description': """This module add new buttons to show the sale in the purchase and vice versa""",
+    'author': "NextaDS",
+    'website': "https://www.nextads.es",
+    'category': 'Uncategorized',
+    'version': '15.0.0.1',
+    'license': "LGPL-3",
+    'depends': [
+        'sale_purchase',
+    ],
+
+    'data': [
+    ],
+
+    'installable': True
+}
