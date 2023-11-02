eb5e43328daddff87ad3039f14e7df2e8a83d0ed
jbasiero <jbasiero@gmail.com>
2023-11-02 12:44:44 +0100

    [ADD] no_tax_sale_and_invoice

---
+++ report_saleorder_and_invoice_document_no_tax/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# (c) 2023 Nexta - Jaume Basiero <jbasiero@nextads.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/a
+{
+    'name': "Sin impuestos",
+
+    'summary': """
+        Este módulo elimina los impuestos en los reports de ventas y facturación""",
+
+    'description': """
+        Este módulo añade modificaciones a los reports:
+            · Sale
+            · Account
+            
+            
+    """,
+
+    'author': "NextaDS",
+    'website': "http://www.nextads.es",
+    'license': "LGPL-3",
+
+    'category': 'Account',
+    'version': '15.0.1',
+
+    'depends': ['sale',
+                'account'
+                ],
+
+    'data': [
+        'report/report_saleorder_document_custom.xml',
+        'report/report_invoice_document_custom.xml',
+
+
+    ],
+
+}
