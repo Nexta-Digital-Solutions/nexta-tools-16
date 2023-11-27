6b0bcf7f0efab01116613e25dd516fcdeb4e66cf
jbasiero <jbasiero@gmail.com>
2023-11-27 15:05:55 +0100

    [RMV] invoice_note

--- delivery_note_linked/models/__init__.py
+++ delivery_note_linked/models/__init__.py
@@ -2,4 +2,4 @@
 # (c) 2023 Nexta - Jaume Basiero <jbasiero@nextads.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/a
 
-from . import SaleOrder, AccountMove, PurchaseOrder, StockPicking
+from . import SaleOrder, PurchaseOrder, StockPicking
