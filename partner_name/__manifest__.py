ace2071142845b955f92b0a662c52057217e8dcb
jbasiero <jbasiero@gmail.com>
2024-01-11 13:10:08 +0100

    [ADD] partner_name

---
+++ partner_name/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (c) 2023 Nexta - Jaume Basiero <jbasiero@nextads.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/a
+{
+    "name": "Nombre sin parentesis",
+    "summary": "Este módulo elimina la función _name_get para mostrar la concatenación del nombre de la empresa y el individual."
+               " Quedando como resultado el nombre sin parentesis, o nombre individual en lugar del nombre de la empresa",
+    'description': """
+       "Este módulo elimina la función _name_get para mostrar la concatenación del nombre de la empresa y el individual."
+               " Quedando como resultado el nombre sin parentesis, o nombre individual en lugar del nombre de la empresa",
+    """,
+    "version": ".0.0.1",
+    "category": "Contacts",
+    "license": "AGPL-3",
+    'author': "NextaDS",
+    'website': "https://www.nextads.es",
+    "depends": ["l10n_es_partner", "contacts"],
+    "data": [
+    ],
+    "installable": True,
+}
