740a941ed54412afd9e506f0c2447b50a439e045
jbasiero <jbasiero@gmail.com>
2024-01-17 13:48:11 +0100

    [MOD] date_not_remaining_days

---
+++ multicompany_color/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# (c) 2023 Nexta - Jaume Basiero <jbasiero@nextads.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/a
+{
+    'name': "Multicompany color",
+
+    'summary': """
+        Este módulo añade diferentes colores de la barra de navegación de odoo, que cambia en función de la empresa que esté activa""",
+
+    'description': """
+        Este módulo añade diferentes colores de la barra de navegación de odoo, que cambia en función de la empresa que esté activa""",
+
+    'author': "NextaDS",
+    'website': "http://www.nextads.es",
+    'license': "LGPL-3",
+
+    'category': '',
+    'version': '16.0.1',
+
+    'depends': ['web'
+                ],
+
+    'data': [
+        'views/layout_multicompany_color.xml',
+    ],
+}
