91f994e2c5a1138e641a3a28ed4ddb33706f1c9a
jbasiero <jbasiero@gmail.com>
2024-07-11 15:02:13 +0200

    [MOD] T3637

--- stock_picking_partner_ref/models/purchase_order.py
+++ stock_picking_partner_ref/models/purchase_order.py
@@ -12,8 +12,11 @@ class PurchaseOrder(models.Model):
     def _prepare_invoice(self):
         invoice_vals = super(PurchaseOrder, self)._prepare_invoice()
 
-        partner_refs = self.picking_ids.filtered(lambda picking: picking.partner_ref != False).mapped('partner_ref')
+        picking_ids = self.env.context.get('active_ids', [])
+        # partner_refs = self.picking_ids.filtered(lambda picking: picking.partner_ref != False).mapped('partner_ref')
+        partner_refs = self.picking_ids.filtered(lambda picking: picking.id in picking_ids and picking.partner_ref).mapped('partner_ref')
         partner_reference = ', '.join(partner_refs)
+
         invoice_vals.update({'partner_ref': partner_reference})
         return invoice_vals
 
